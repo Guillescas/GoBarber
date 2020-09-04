ccc7c3b6260351e859dbdaf4a293544c152c6ddc
Guilherme Illescas <gui.illescas@gmail.com>
2020-09-04 07:49:33 -0300

    Create error class and appling this on application

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -1,6 +1,8 @@
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 
+import AppError from '../errors/AppError';
+
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
@@ -23,7 +25,7 @@ class CreateAppointmentService {
     );
 
     if (findAppointmentsInSameDate) {
-      throw Error('This appointmant is already booked');
+      throw new AppError('This appointmant is already booked', 400);
     }
 
     const appointment = appointmentsRepository.create({
