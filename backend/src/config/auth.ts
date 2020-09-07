b0c2f410e8d30f7cf348d139dc4bbe96e1b816d7
Guilherme Illescas <gui.illescas@gmail.com>
2020-09-07 15:20:51 -0300

    Restructuring folder organization

---
+++ backend/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'oasidfugo73g87qbyrqo783rbwo',
+    expiresIn: '2d',
+  },
+};
--- src/config/auth.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  jwt: {
-    secret: 'oasidfugo73g87qbyrqo783rbwo',
-    expiresIn: '2d',
-  },
-};
