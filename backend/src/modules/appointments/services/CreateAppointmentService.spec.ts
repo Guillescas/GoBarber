1794bfd31f013160371dbd3109c404a8d66953d3
Guilherme Illescas <gui.illescas@gmail.com>
2020-11-30 18:11:39 -0300

    Appointment service test completed

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,8 +1,20 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
-  it('Should be able to reate a new appointment', () => {
-    expect(1 + 2).toBe(3);
+  it('Should be able to reate a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointments = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointments.execute({
+      date: new Date(),
+      provider_id: '123123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123');
   });
 
   it('Should not be able to reate two appointments on the same time', () => {
