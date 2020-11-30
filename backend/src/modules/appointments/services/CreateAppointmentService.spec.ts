fc6cbad21cf9aff8409847ef3abb9022d0f84c57
Guilherme Illescas <gui.illescas@gmail.com>
2020-11-30 17:10:04 -0300

    Create first test (to learn only)

---
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,11 @@
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('Should be able to reate a new appointment', () => {
+    expect(1 + 2).toBe(3);
+  });
+
+  it('Should not be able to reate two appointments on the same time', () => {
+    expect(1 + 2).toBe(3);
+  });
+});
