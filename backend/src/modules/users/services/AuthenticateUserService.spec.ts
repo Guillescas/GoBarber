0b2ff1f888996e9fb2f8231d8ebb7ae5013a4b87
Guilherme Illescas <gui.illescas@gmail.com>
2020-12-01 17:06:20 -0300

    Create test for authenticate service

---
+++ backend/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -0,0 +1,35 @@
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+import AuthenticateUserService from './AuthenticateUserService';
+import CreateUserService from './CreateUserService';
+
+describe('AuthenticateUser', () => {
+  it('Should be able to authenticate', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    const user = await createUser.execute({
+      name: 'John Doe',
+      email: 'john.doe@gmail.com',
+      password: '123123',
+    });
+
+    const response = await authenticateUser.execute({
+      email: 'john.doe@gmail.com',
+      password: '123123',
+    });
+
+    expect(response).toHaveProperty('token');
+    expect(response.user).toEqual(user);
+  });
+});
