4c20255e2546ae885c4a63904982f0bde5067fab
Guilherme Illescas <gui.illescas@gmail.com>
2020-12-02 22:21:14 -0300

    Create test to not be able to recover non-existing user

--- backend/src/modules/users/services/SendForgotPassowordEmailService.spec.ts
+++ backend/src/modules/users/services/SendForgotPassowordEmailService.spec.ts
@@ -1,4 +1,4 @@
-// import AppError from '@shared/errors/AppError';
+import AppError from '@shared/errors/AppError';
 
 import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
@@ -27,5 +27,21 @@ describe('SendForgotPasswordEmail', () => {
     });
 
     expect(sendMail).toHaveBeenCalled();
+  });
+
+  it('Should not be able to recover a non-existing user password', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeMailProvider = new FakeMailProvider();
+
+    const sendForgotPasswordEmail = new SendForgotPassowordEmailService(
+      fakeUsersRepository,
+      fakeMailProvider,
+    );
+
+    await expect(
+      sendForgotPasswordEmail.execute({
+        email: 'john.doe@gmail.com',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
